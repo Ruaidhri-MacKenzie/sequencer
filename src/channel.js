d9fe56250975e0a9f86344515b54dac9a3113121
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2021-04-24 19:15:43 +0100

    Added Synth and Sequence instances to each Channel

--- src/channel.js
+++ src/channel.js
@@ -1,29 +1,31 @@
 import Sequence from "./sequence.js";
+import Synth from "./synth.js";
 
 export default class Channel {
-	constructor(elements, resetSoloAll) {
+	constructor(context, index, resetSoloAll) {
+		this.context = context;
+		this.index = index;
+		this.resetSoloAll = resetSoloAll;
+
 		this.level = 75;
 		this.pan = 0;
 		this.patch = "";
 		this.isMute = false;
 		this.isSolo = false;
 
-		this.$sequence = elements.$sequence;
-		this.$level = elements.$level;
-		this.$pan = elements.$pan;
-		this.$patch = elements.$patch;
-		this.$mute = elements.$mute;
-		this.$solo = elements.$solo;
+		this.$element = document.getElementById(`channel${this.index}`);
+		this.$level = this.$element.querySelector(".channel__level");
+		this.$pan = this.$element.querySelector(".channel__pan");
+		this.$patch = this.$element.querySelector(".channel__patch");
+		this.$mute = this.$element.querySelector(".channel__mute");
+		this.$solo = this.$element.querySelector(".channel__solo");
 
-		this.sequence = new Sequence(this.$sequence);
-		this.synth = {};
-
-		this.resetSoloAll = resetSoloAll;
+		this.sequence = new Sequence(this.context, this.index);
+		this.synth = new Synth(this.context, this.index);
 
 		this.$level.onchange = this.setLevel.bind(this);
 		this.$pan.onchange = this.setPan.bind(this);
 		this.$patch.onchange = this.setPatch.bind(this);
-
 		this.$mute.onclick = this.toggleMute.bind(this);
 		this.$solo.onclick = this.toggleSolo.bind(this);
 	}
