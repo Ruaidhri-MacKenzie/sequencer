654e62bf92aa0bbca2795bb0c256079efb40cdf0
Ruaidhri-MacKenzie <rmackenzie88@protonmail.com>
2021-04-26 02:40:42 +0100

    Renamed gain to envelope, added frequency filter

--- src/synth.js
+++ src/synth.js
@@ -3,38 +3,59 @@ export default class Synth {
 		this.context = context;
 		this.index = index;
 
-		this.patch = {
-			oscillatorType: "sine",
+		this.oscillator = {
+			type: "sine",
 			pitch: 261.6,
 		};
 
-		this.oscillator = {};
-		this.filter = {};
-		this.lfo = {};
-		this.envelope = {
-			attack: 0.1,
-			release: 0.1,
+		this.filter = {
+			type: "low-pass",
+			cutoff: 500,
+			resonance: 0,
 		};
-		this.master = {};
+
+		this.lfo = {
+			depth: 0,
+			speed: 1,
+		};
+
+		this.envelope = {
+			attack: 0,
+			decay: 0,
+			sustain: 1,
+			release: 0,
+		};
 	}
+
+	loadPatch(patch) {}
 
 	play(time) {
 		const duration = 4 / 16; // 4 beats in sixteenth notes
 
+		// Create Oscillator
 		const oscillator = this.context.createOscillator();
-		oscillator.type = this.patch.oscillatorType || "sine";
-		oscillator.frequency.value = this.patch.pitch || 261.6;
+		oscillator.type = this.oscillator.type;
+		oscillator.frequency.value = this.oscillator.pitch;
 
-		const gain = this.context.createGain();
-		gain.gain.cancelScheduledValues(time);
-		gain.gain.setValueAtTime(0, time);
-		gain.gain.linearRampToValueAtTime(1, time + this.envelope.attack);
-		gain.gain.linearRampToValueAtTime(0, time + duration - this.envelope.release);
+		// Create Frequency Filter
+		const filter = this.context.createBiquadFilter();
+		filter.type = "lowpass";
+		filter.frequency.value = 500;
+		filter.Q.value = 1;
 
-		oscillator.connect(gain);
-		gain.connect(this.context.destination);
+		// Create Amplitude Envelope
+		const envelope = this.context.createGain();
+		envelope.gain.cancelScheduledValues(time);
+		envelope.gain.setValueAtTime(0, time);
+		envelope.gain.linearRampToValueAtTime(1, time + this.envelope.attack);
+		envelope.gain.linearRampToValueAtTime(this.envelope.sustain, time + this.envelope.attack + this.envelope.decay);
+		envelope.gain.linearRampToValueAtTime(0, time + duration + this.envelope.release);
 
+		// Connect nodes in context graph
+		oscillator.connect(filter).connect(envelope).connect(this.context.destination);
+
+		// Playback
 		oscillator.start(time);
-		oscillator.stop(time + duration);
+		oscillator.stop(time + duration + this.envelope.release);
 	}
 }
